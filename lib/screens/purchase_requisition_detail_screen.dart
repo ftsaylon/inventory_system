dccf25c405d98365902bf7fc4d106a0513ba9a6d
Francis Saylon <28182748+ftsaylon@users.noreply.github.com>
2020-01-30 10:08:21 +0800

    Added draft of PDF creation of Purchase Requisition

--- lib/screens/purchase_requisition_detail_screen.dart
+++ lib/screens/purchase_requisition_detail_screen.dart
@@ -1,21 +1,25 @@
 /* -------------------------------- Packages -------------------------------- */
 
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:inventory_system/providers/purchase_requisition.dart';
+import 'package:pdf/pdf.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
+import 'package:pdf/widgets.dart' as pdfLib;
+import 'package:path_provider/path_provider.dart';
 
 /* -------------------------------- Providers ------------------------------- */
 
 import '../providers/purchase_requisitions.dart';
-import '../providers/purchase_requisition.dart';
 
 /* --------------------------------- Screens -------------------------------- */
 
-import 'edit_purchase_requisition_screen.dart';
+import '../screens/edit_purchase_requisition_screen.dart';
+import '../screens/pdf_viewer.dart';
 
 /* --------------------------------- Widgets -------------------------------- */
 
-import '../widgets/purchase_requisition_item_list_tile.dart';
 import 'purchase_requisition_item_list_screen.dart';
 
 class PurchaseRequisitionDetailScreen extends StatelessWidget {
@@ -35,6 +39,12 @@ class PurchaseRequisitionDetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.picture_as_pdf),
+            onPressed: () {
+              _generatePDF(context, loadedRequisition);
+            },
+          ),
           IconButton(
             icon: Icon(Icons.edit),
             onPressed: () => Navigator.of(context).pushNamed(
@@ -144,13 +154,108 @@ class PurchaseRequisitionDetailScreen extends StatelessWidget {
             RaisedButton(
               child: Text("See Items"),
               onPressed: () {
-                Navigator.of(context)
-                    .pushNamed(PurchaseRequisitionItemListScreen.routeName, arguments: loadedRequisition.id);
+                Navigator.of(context).pushNamed(
+                    PurchaseRequisitionItemListScreen.routeName,
+                    arguments: loadedRequisition.id);
               },
-            )
+            ),
+            RaisedButton(
+              child: Text("View PDF"),
+              onPressed: () async {
+                _viewPDF(context, await _getPDFFilePath(loadedRequisition.id));
+              },
+            ),
           ],
         ),
       ),
     );
   }
+
+/* -------------------------------------------------------------------------- */
+/*                                   Helpers                                  */
+/* -------------------------------------------------------------------------- */
+
+  List<List<String>> _getData(PurchaseRequisition requisition) {
+    return [...requisition.purchaseRequisitionItems.items.values.map(
+      (item) => [
+        item.name ?? '',
+        item.preferredSupplier ?? '',
+        item.sku ?? '',
+        item.quantity.toString() ?? '',
+        item.unitType ?? '',
+        item.estimatedPrice ?? '',
+        item.size ?? '',
+        item.isPreApproved.toString() ?? '',
+        item.comments ?? '',
+      ],
+    )];
+  }
+
+  _generatePDF(BuildContext context, PurchaseRequisition data) async {
+    final pdfLib.Document pdf = pdfLib.Document(deflate: zlib.encode);
+
+    pdf.addPage(
+      pdfLib.MultiPage(
+        pageFormat: PdfPageFormat.a4,
+        build: (context) => [
+          pdfLib.Text('Purchase Requisition ID: ${data.id.toString()}'),
+          pdfLib.Text('Location: ${data.location}'),
+          pdfLib.Text('Department: ${data.department}'),
+          pdfLib.Text('Project: ${data.project}'),
+          pdfLib.Text('Total Estimated Cost: "${data.totalEstimatedCost}"'),
+          pdfLib.Text('Requested By: ${data.requestedBy}'),
+          pdfLib.Text('Approver: ${data.nextApprover}'),
+          pdfLib.Text('Status: ${data.status}'),
+          pdfLib.Text(
+              'Date Required: ${DateFormat.yMEd().add_jms().format(data.dateRequired)}'),
+          pdfLib.Text(
+              'Date Submitted: ${DateFormat.yMEd().add_jms().format(data.dateSubmitted)}'),
+          pdfLib.Text(
+              'Date Modified: ${DateFormat.yMEd().add_jms().format(data.dateModified)}'),
+          pdfLib.Table.fromTextArray(
+            context: context,
+            data: [
+              [
+                'Name',
+                'Preferred Supplier',
+                'SKU',
+                'Quantity',
+                'Unit Type',
+                'Estimated Price',
+                'Size',
+                'Pre-Approved',
+                'Comments',
+              ],
+              ..._getData(data)
+            ],
+          ),
+        ],
+      ),
+    );
+
+    final File file = File(await _getPDFFilePath(data.id));
+    print(file.path);
+    await file.writeAsBytes(pdf.save());
+  }
+
+/* -------------------------------------------------------------------------- */
+
+  _viewPDF(BuildContext context, String path) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) => PdfViewerPage(path: path),
+      ),
+    );
+  }
+
+/* -------------------------------------------------------------------------- */
+
+  Future<String> _getPDFFilePath(String id) async {
+    final String dir = (await getApplicationDocumentsDirectory()).path;
+    final String path = '$dir/PR-$id.pdf';
+
+    return path;
+  }
+
+/* -------------------------------------------------------------------------- */
 }
